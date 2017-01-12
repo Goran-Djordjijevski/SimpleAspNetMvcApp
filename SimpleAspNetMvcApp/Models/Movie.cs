f51fa05ea5963a56e4fa0967b5593a1b5a5ad002
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-01-12 10:55:12 +0100

    Add project files.

---
+++ SimpleAspNetMvcApp/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleAspNetMvcApp.Models
+{
+    public class Movie
+    {
+        public Movie()
+        {
+
+        }
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the title of the movie.")]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Enter the genre of the movie.")]
+        [RegularExpression((@"^[A-Z]+[a-zA-Z''-'\s]*$"))]
+        [StringLength(20)]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Please enter the movie rating.")]
+        [StringLength(10)]
+        public double Rating { get; set; }
+    }
+}
---
+++ SimpleAspNetMvcApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleAspNetMvcApp.Startup))]
+namespace SimpleAspNetMvcApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
